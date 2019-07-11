e2f8143a820596f53302b255fd9d8ed4468f8d2c
MayaraFreitas <mah.alvesfreitas@hotmail.com>
2019-07-10 21:10:12 -0300

    Criando Projetos + Agrupando na solução + Criando Entidades

---
+++ ProAgil.Domain/Evento.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace ProAgil.Domain
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public DateTime DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string ImagemURL { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public List<Lote> Lote { get; set; }
+        public List<RedeSocial> RedesSociais { get; set; }
+        public List<PalestranteEvento> PalestranteEventos { get; set; }
+    }
+}
